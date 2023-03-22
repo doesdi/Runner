8b4bbe9a1702739d4166b34311b3c9e18375cb33
Evgeniy Samoilov <husokka@mail.ru>
2023-03-22 15:19:52 +0300

    fourth

---
+++ My project/Assets/Scripts/Spawn2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn2 : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject Enemy;
+    public float time;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy1());
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(SpawnEnemy1());
+    }
+
+    IEnumerator SpawnEnemy1()
+    {
+        yield return new WaitForSeconds(time);
+        var newObj = (GameObject)Instantiate(Enemy, SpawnPos.position, Quaternion.identity);
+        Destroy(newObj, time + 40);
+
+        Repeat();
+    }
+}
